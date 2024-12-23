d439c68000d93564b174815a79f0874f155805b9
Amgaa <enkh-amgalan@data-artist.com>
2024-12-23 14:02:31 +0800

    bingo api

---
+++ app/api/bingo/route.ts
@@ -0,0 +1,32 @@
+const bingos: number[] = []
+
+export async function GET() {
+  if (bingos.length === 75) {
+    return new Response(JSON.stringify({ bingo: null }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+
+  const min = 1
+  const max = 75
+  let bingo = arbitraryRandom(min, max)
+  
+  while (bingos.includes(bingo)) {
+    bingo = arbitraryRandom(min, max)
+  }
+  
+  bingos.push(bingo)
+
+  return new Response(JSON.stringify({ bingo: bingo }), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+function arbitraryRandom(min: number, max: number) {
+  const arbitrary_random = Math.random() * (max - min) + min
+  return Math.floor(arbitrary_random)
+}
