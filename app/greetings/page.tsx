e10ae27cf97b43a90496d5d5d5ce20f212850ac8
Amgaa <enkh-amgalan@data-artist.com>
2024-12-23 14:26:20 +0800

    device id, is mobile

--- app/greetings/page.tsx
+++ app/greetings/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+'use client'
+import { DeviceUUID } from 'device-uuid'
+
+export default function Greetings() {
+  const { isMobile } = new DeviceUUID().parse()
+  const deviceId = new DeviceUUID().get()
+  console.log(isMobile, deviceId)
   return (
     <div className="flex">
       Greetings
